45a91719757693966775849669897f3deee59a9b
bpernot <pernot.benjamin@outlook.fr>
2026-03-20 13:55:00 +0100

    Feat: add tailwind and displaying the fetched users

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84b32c3c781b53fdf40bfeab3d5c136575c9709c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f96621d371bca3bebd7edcd5ba821226742344a")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
